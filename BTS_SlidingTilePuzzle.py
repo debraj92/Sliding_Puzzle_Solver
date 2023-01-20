b53861c9979b12bf31714e0066b31d1266a62d0d
Debraj Ray <debrajpesit@gmail.com>
2023-01-19 20:25:49 -0700

    Track path in BTS

--- BTS_SlidingTilePuzzle.py
+++ BTS_SlidingTilePuzzle.py
@@ -38,40 +38,49 @@ class BTS_SlidingTilePuzzle:
         return newHeuristic - oldHeuristic
 
     def limitedDfs(self, gameState: SlidingTileBoard, pathCost: int, costLimit: int, nodesLimit: int,
-                   parent_move_destination: list):
+                   path: list, parent_move_destination: list):
         g_cost = pathCost + gameState.gCost()
         f = g_cost + gameState.heuristic
         if self.solutionCost == self.solutionLowerBound:
-            return
+            return False
         elif f > costLimit:
             self.f_above = min(self.f_above, f)
-            return
+            return False
         elif f >= self.solutionCost:
             self.f_below = self.solutionCost
-            return
+            return False
         else:
             self.f_below = max(f, self.f_below)
 
         if self.nodes >= nodesLimit:
-            return
+            return False
 
         if gameState.isSolved():
             self.solutionCost = f
-            return
+            path.append(gameState.serializeBoardToString())
+            return True
 
         validMoves = gameState.generateValidMoves(parent_move_destination)
         validMoves = sorted(validMoves, key=lambda m: self.sortFunction(m, gameState))
 
         for move in validMoves:
             moved = gameState.move(move)
-            self.limitedDfs(gameState, g_cost, costLimit, nodesLimit, move[1])
+
+            found = self.limitedDfs(gameState, g_cost, costLimit, nodesLimit, path, move[1])
+            if found:
+                path.append(gameState.serializeBoardToString())
             gameState.undoMove(move, moved)
 
-    def search(self, gameState: SlidingTileBoard, costLimit, nodeLimit):
+            if found:
+                return found
+
+        return False
+
+    def search(self, gameState: SlidingTileBoard, costLimit: int, nodeLimit: int, path: list):
         self.f_below = 0
         self.f_above = self.INFINITY
         self.nodes = 0
-        self.limitedDfs(gameState, 0, costLimit, nodeLimit, [-1, -1])
+        self.limitedDfs(gameState, 0, costLimit, nodeLimit, path, [-1, -1])
         if self.nodes >= nodeLimit:
             return [0, self.f_below]
         elif self.f_below >= self.solutionCost:
@@ -79,7 +88,7 @@ class BTS_SlidingTilePuzzle:
         else:
             return [self.f_above, self.INFINITY]
 
-    def solveWithBts(self, gameState: SlidingTileBoard):
+    def solveWithBts(self, gameState: SlidingTileBoard, path: list):
         self.fCostBound = [gameState.heuristic, self.INFINITY]
         self.solutionCost = self.INFINITY
         self.nodeBudget = 0
@@ -91,7 +100,7 @@ class BTS_SlidingTilePuzzle:
             '''
             Regular IDA*
             '''
-            self.fCostBound = self.search(gameState, self.fCostBound[0], self.INFINITY)
+            self.fCostBound = self.search(gameState, self.fCostBound[0], self.INFINITY, path)
             if self.nodes >= (self.c1 * self.nodeBudget):
                 self.nodeBudget = self.nodes
                 continue
@@ -123,9 +132,21 @@ class BTS_SlidingTilePuzzle:
     def solveAllGamesWithBTS(self):
         start_time = time.time()
         game = SlidingTileBoard(self.games[0])
-        self.solveWithBts(game)
+        path = []
+        self.solveWithBts(game, path)
         duration = time.time() - start_time
         print("--- %s s ---" % duration)
         if self.solutionCost < self.INFINITY:
             print('Solution Cost ', self.solutionCost)
-            game.printBoard()
+            self.printSolution(path)
+
+    def printSolution(self, path):
+        path.reverse()
+        print()
+        for tileBoard in path:
+            tileBoard = tileBoard.strip('|').split('|')
+            gameTiles = [eval(i) for i in tileBoard]
+            slidingTileBoardInstance = SlidingTileBoard(gameTiles)
+            slidingTileBoardInstance.printBoard()
+
+        print('Total moves ', len(path))
