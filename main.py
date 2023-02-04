0d4592ce8b47d95af60843f6dd82d618ac3301f0
Debraj Ray <debrajpesit@gmail.com>
2023-02-04 09:40:21 -0700

    Moved py files to its own folder

--- main.py
+++
@@ -1,9 +0,0 @@
-from SlidingTileGame import SlidingTileGame
-from BTS_SlidingTilePuzzle import BTS_SlidingTilePuzzle
-
-#game = SlidingTileGame()
-#game.solveAllGamesWithIDA_Star()
-
-game = BTS_SlidingTilePuzzle()
-game.solveAllGamesWithBTS()
-
